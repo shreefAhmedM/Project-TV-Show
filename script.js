ba28982eeb9905419b32eb5e2608f81a496b68d2
shreefAhmedM <livecoding.33@gmail.com>
2025-11-06 10:08:17 +0000

    level one for TV-show project has completed

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-//You can edit ALL of the code here
+function zeroPad(num) {
+  return num.toString().padStart(2, "0");
+}
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
-
-function makePageForEpisodes(episodeList) {
+function makePageForEpisodes(episodes) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.textContent = "";
+  episodes.forEach((episode) => {
+    const epiDiv = document.createElement("div");
+    epiDiv.className = "epiCard";
+    const episodeCode = `S${zeroPad(episode.season)}E${zeroPad(
+      episode.number
+    )}`;
+
+    const titleElem = document.createElement("h2");
+    titleElem.textContent = `${episode.name} (${episodeCode})`;
+    epiDiv.appendChild(titleElem);
+    if (episode.image && episode.image.medium) {
+      const imageElem = document.createElement("img");
+      imageElem.src = episode.image.medium;
+      imageElem.alt = `${episode.name} image`;
+      epiDiv.appendChild(imageElem);
+
+      const summaryElem = document.createElement("p");
+      summaryElem.innerHTML = episode.summary;
+      epiDiv.appendChild(summaryElem);
+      rootElem.appendChild(epiDiv);
+    }
+  });
 }
 
 window.onload = setup;
